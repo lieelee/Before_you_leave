b50d5ef353eba11ce3e3d6c7072cc33287ab3fb3
lieelee <104736314+lieelee@users.noreply.github.com>
2022-06-05 21:55:22 -0700

    Delete config.js

--- src/hooks/yelp-api/config.js
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL = "https://api.yelp.com/v3";
-
-//Fix and remove it later this token.
-// never ever save save a bearer token inside the frontend code
-export const BEARER_TOKEN =
-  "esXMIabMu5oPNg4GHFyvb1P_Igih5xs488OC8sYBf7zlm49IRG6Fx2FZAKZ6JkuVA7UPtwZEyXtfbdqMTyw8ZRuhjCMjHO-KLccBPl4efSYOM1qYKornZjjcy7CAYnYx";
