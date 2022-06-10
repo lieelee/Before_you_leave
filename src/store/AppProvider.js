fc2b9cbcb66078d09bc624b6bef98ee6f85bc2fb
lieelee <104736314+lieelee@users.noreply.github.com>
2022-06-10 16:48:53 -0700

    Update AppProvider.js

--- src/store/AppProvider.js
+++ src/store/AppProvider.js
@@ -2,7 +2,6 @@ import React, { useReducer, useState } from "react";
 import AppContext from "./app-context";
 
 // const api = {
-//   key: "2a470a5ce69a36ec5dc341371a7e9713",
 //   base: "https://api.openweathermap.org/data/2.5/",
 // };
 
